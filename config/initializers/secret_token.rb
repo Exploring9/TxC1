c4bb1259f6eeee7b0def2a99939778c82bd20b72
nv <milukas.matas@gmail.com>
2014-04-09 21:05:49 +0100

    Second

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TxC1::Application.config.secret_key_base = '4408c6cb81d9dacc92eac504d881e008d4caf78a8e4ad8b422d9bc5f1797fb03ec9b4174754a01a6128d4ee7bc60d9eb159ee658d82cc18ae5fd7f719eff4a69'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TxC1::Application.config.session_store :cookie_store, key: '_TxC1_session'
